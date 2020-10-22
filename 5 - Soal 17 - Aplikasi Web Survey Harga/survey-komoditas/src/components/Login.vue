0b0548a4fbf6754af36188af442a0af6a315cd4e
oxwazz <muhammad.rahmahalim@gmail.com>
2020-10-22 20:00:51 +0700

    update ERD soal 17

--- 5 - Soal 17 - Aplikasi Web Survey Harga/survey-komoditas/src/components/Login.vue
+++ 5 - Soal 17 - Aplikasi Web Survey Harga/survey-komoditas/src/components/Login.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>Login Page</h1>
+    <div class="contaiter w-25 ml-auto mr-auto">
+    <h1 class="text-center mt-3">- Login Page -</h1>
+    <hr>
+    <form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <button @click.prevent="" type="submit" class="btn btn-primary w-100">Submit</button>
+    </form>
+    </div>
 </template>
